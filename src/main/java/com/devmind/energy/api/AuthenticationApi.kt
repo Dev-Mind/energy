a51f16c0bea62daf6a22cb9d2276e742b2f1148d
guillaumeehret <guillaume@dev-mind.fr>
2026-07-27 15:51:26 +0200

    Add redirect endpoint for Enedis account and update token API path

--- src/main/java/com/devmind/energy/api/AuthenticationApi.kt
+++ src/main/java/com/devmind/energy/api/AuthenticationApi.kt
@@ -1,15 +1,28 @@
 package com.devmind.energy.api
 
+import com.devmind.energy.EnergyProperties
 import com.devmind.energy.service.TokenService
 import com.devmind.energy.service.dto.EnedisTokenResponseDto
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.servlet.view.RedirectView
 
 @RestController
-@RequestMapping("/api/enedis/token")
-class AuthenticationApi(private val tokenService: TokenService) {
-    @GetMapping
+@RequestMapping("/api/enedis")
+class AuthenticationApi(private val tokenService: TokenService, private val properties: EnergyProperties) {
+    @GetMapping("/token")
     fun token(): EnedisTokenResponseDto =
         tokenService.token ?: throw IllegalArgumentException("Token not found")
+
+    @GetMapping("/account")
+    fun redirectToEnedisAccount(): RedirectView {
+        val clientId = properties.clientId
+        val duration = "P3Y"
+        val state = "devmind-energy-app"
+        return RedirectView(
+            "https://mon-compte-particulier.enedis.fr/dataconnect/v1/oauth2/authorize?client_id=$clientId&duration=$duration&response_type=code&state=$state"
+        )
+    }
 }
